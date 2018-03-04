9bf78c281ae3b1272e3f58555bd5e8c4cd13e7d8
Леонов Дмитрий Игоревич <d.leonov@ntc-vulkan.ru>
2018-03-04 20:25:36 +0300

    refactoring

--- humidity_controller/humidity_controller.ino
+++ humidity_controller/humidity_controller.ino
@@ -1,3 +1,5 @@
+#include "psychrometric.hpp"
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,30 +8,4 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-}
-
-float computeHumidity(float dryT, float wetT)
-{
-  float vaporPressure = computeVaporPressure(dryT,wetT);
-  float saturationPressure = computeSaturationPressure(dryT);
-  return 100*vaporPressure/saturationPressure;
-}
-
-float computeVaporPressure(float dryT, float wetT)
-{
-  const double A = 0.0007947;
-  const int p = 1000;
-  const double a = 0.00115;
-  float saturationPressureWet = computeSaturationPressure(wetT);
-  float vaporPressure = saturationPressureWet - A*p*(dryT-wetT)*(1+a*wetT);
-  return vaporPressure;
-}
-
-float computeSaturationPressure(float t)
-{
-  const double E = 6.1121;
-  const double a = 17.5043;
-  const double b = 241.2;
-  float pressure = E * exp(abs(a*t/(b+t)));
-  return pressure;
 }
